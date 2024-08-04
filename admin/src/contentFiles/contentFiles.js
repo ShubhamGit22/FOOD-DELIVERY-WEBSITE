c6f985d8d456370ac0d610da0ad5565a916af0d9
Shubham Ashok Chavan <139915555+ShubhamGit22@users.noreply.github.com>
2024-08-04 11:19:06 +0530

    Update contentFiles.js

--- admin/src/contentFiles/contentFiles.js
+++ admin/src/contentFiles/contentFiles.js
@@ -6,7 +6,7 @@ import upload_area from './upload_area.png'
 import parcel_icon from './parcel_icon.png'
 
  // export const url = 'http://localhost:4000'
-export const url = "https://mern-food-delivery-website.onrender.com"                                        //backend url after deploying on render.
+export const url = "https://backend-mern-food-delivery-website.onrender.com"                                        //backend url after deploying on render.
 export const contentFiles ={
     logo,
     add_icon,
